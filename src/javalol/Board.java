49451d248df346d22d7467b5bd3e8147bf03be4e
Daehyun Nam <namdhyun@gmail.com>
2014-05-15 02:15:55 +0900

    initial-making class for Board layout

---
+++ src/javalol/Board.java
@@ -0,0 +1,18 @@
+package javalol;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Board extends Component{
+	private int width = 550;
+	private int height = 570;
+	
+	//Constructor method
+	public Board(String one, String two){
+		
+	}
+	
+	//the method to draw board
+	public void paint(Graphics g){
+	}
+}
