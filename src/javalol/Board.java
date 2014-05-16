3f513dc29e954733c1e1b34612043cded14c868c
Daehyun Nam <namdhyun@gmail.com>
2014-05-17 01:55:52 +0900

    added the part of button location, yet not eventListener

--- src/javalol/Board.java
+++ src/javalol/Board.java
@@ -4,8 +4,8 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Board extends JFrame{
-	private int width = 880;
-	private int height = 660;
+	private int width = 1200;
+	private int height = 800;
 	
 	//Constructor method
 	public Board(){
@@ -19,9 +19,15 @@ public class Board extends JFrame{
 		//전반적인 레이아웃 짜는 부분
 		ImageIcon image = new ImageIcon("images/brand.png");
 		JLabel imageLabel = new JLabel(image);
-		//imageLabel.setLocation(5, 6);
+		imageLabel.setLocation(5, 6);
 		imageLabel.setSize(200, 200);
 		add(imageLabel);
+		
+		//버튼 테스트해보았습니다
+		JButton btn1 = new JButton("주사위굴리기");
+		btn1.setLocation(6, 221);
+		btn1.setSize(120, 30);
+		add(btn1);
 		
 		//레이아웃 다짠 후에 패널 부착!
 		Container contentPane = getContentPane();
