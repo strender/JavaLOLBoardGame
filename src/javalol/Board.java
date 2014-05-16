219648733edb2f4343071c60f3d3ccb05a522a11
Daehyun Nam <namdhyun@gmail.com>
2014-05-17 01:35:20 +0900

    tested for image insertion, and succeded

--- src/javalol/Board.java
+++ src/javalol/Board.java
@@ -3,16 +3,31 @@ package javalol;
 import java.awt.*;
 import javax.swing.*;
 
-public class Board extends Component{
-	private int width = 550;
-	private int height = 570;
+public class Board extends JFrame{
+	private int width = 880;
+	private int height = 660;
 	
 	//Constructor method
-	public Board(String one, String two){
+	public Board(){
+		setTitle("LOLBoardGame");
+		setSize(width, height);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(null);
+		setResizable(false);
+		
+		
+		//전반적인 레이아웃 짜는 부분
+		ImageIcon image = new ImageIcon("images/brand.png");
+		JLabel imageLabel = new JLabel(image);
+		//imageLabel.setLocation(5, 6);
+		imageLabel.setSize(200, 200);
+		add(imageLabel);
+		
+		//레이아웃 다짠 후에 패널 부착!
+		Container contentPane = getContentPane();
+		contentPane.setBackground(Color.WHITE);
+		
 		
 	}
 	
-	//the method to draw board
-	public void paint(Graphics g){
-	}
 }
