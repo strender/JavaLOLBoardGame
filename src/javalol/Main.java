f006e32075848a38ca2df649367094857bd1059f
Daehyun Nam <namdhyun@gmail.com>
2014-05-17 01:37:52 +0900

    modified the Main file for checking the MVC operated exactly

--- src/javalol/Main.java
+++ src/javalol/Main.java
@@ -1,10 +1,13 @@
 package javalol;
 
+import javax.swing.JFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Board board = new Board();
+		board.setVisible(true);
 	}
 
 }
