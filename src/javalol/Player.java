f3daf1c925e67cfa874a55bf2f3d4c7eebb37f7b
Daehyun Nam <namdhyun@gmail.com>
2014-05-19 12:53:54 +0900

    made skleton code for object Player

---
+++ src/javalol/Player.java
@@ -0,0 +1,24 @@
+package javalol;
+
+public class Player {
+	public String Name;
+	public int position = 0;
+	public int PlayerNum;
+	public boolean disconnect = false;
+	public int turns = 0;
+	
+	public Player(String name, int num){
+		Name = name;
+		PlayerNum = num;
+	}
+	
+	public void Move(){
+		
+	}
+	
+	public void drawPlayer(){
+		
+	}
+	
+	
+}
