1de699640b48cc4c20f8fa16ac7e427010aec1dd
Daehyun Nam <namdhyun@gmail.com>
2014-05-23 01:40:20 +0900

    added method setName()

--- src/javalol/Player.java
+++ src/javalol/Player.java
@@ -1,11 +1,18 @@
 package javalol;
 
+import javax.swing.JOptionPane;
+
 public class Player {
 	public String Name;
 	public int position = 0;
 	public int PlayerNum;
 	public boolean disconnect = false;
 	public int turns = 0;
+	
+	public void setName(){
+		String s = JOptionPane.showInputDialog("Player1 이름 입력:");
+		this.Name = s;
+	}
 	
 	public Player(String name, int num){
 		Name = name;
@@ -18,7 +25,5 @@ public class Player {
 	
 	public void drawPlayer(){
 		
-	}
-	
-	
+	}	
 }
