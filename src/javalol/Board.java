23267135b63dcc6cd5d939ff5340c33b69a19d85
Daehyun Nam <namdhyun@gmail.com>
2014-05-23 16:25:59 +0900

    removed unused import

--- src/javalol/Board.java
+++ src/javalol/Board.java
@@ -8,8 +8,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.border.Border;
 
