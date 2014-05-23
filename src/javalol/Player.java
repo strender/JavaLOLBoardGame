a8259caecaf548966f13befd113da05820970db5
Daehyun Nam <namdhyun@gmail.com>
2014-05-23 10:31:40 +0900

    set the basic HP and money, changed the constructor method

--- src/javalol/Player.java
+++ src/javalol/Player.java
@@ -6,17 +6,21 @@ public class Player {
 	public String Name;
 	public int position = 0;
 	public int PlayerNum;
+	public int money;
+	public int health;
 	public boolean disconnect = false;
 	public int turns = 0;
 	
 	public void setName(){
-		String s = JOptionPane.showInputDialog("Player1 이름 입력:");
+		String s = JOptionPane.showInputDialog("Player" + PlayerNum + " 이름 입력:");
 		this.Name = s;
 	}
 	
-	public Player(String name, int num){
-		Name = name;
+	public Player(int num){
 		PlayerNum = num;
+		setName();
+		money = 475;
+		health = 550;
 	}
 	
 	public void Move(){
