f81c89a053319dbf9d391db4025efb9b8ab99f87
Daehyun Nam <namdhyun@gmail.com>
2014-05-26 02:18:13 +0900

    initial commit & class for buttonEventListener

---
+++ src/javalol/ButtonListener.java
@@ -0,0 +1,14 @@
+package javalol;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
