a1757d981a81122e92daba9a145b1aec7c3fc4e6
Daehyun Nam <namdhyun@gmail.com>
2014-05-26 11:16:04 +0900

    edited variable and constructor method, but not yet decided whether to use this

--- src/javalol/Block.java
+++ src/javalol/Block.java
@@ -1,8 +1,24 @@
 package javalol;
 
-public class Block {
+public class Block{
 	String location_name;
 	boolean is_special;
-	int[] price = new int[4];
+	int[] price = new int[5];
+	int x_position, y_position;
 	
+	public Block(){
+		
+	}
+	
+	public Block(String s, boolean special, int p0, int p1, int p2, int p3, int p4, int x, int y){
+		location_name = s;
+		is_special = special;
+		price[0] = p0;
+		price[1] = p1;
+		price[2] = p2;
+		price[3] = p3;
+		price[4] = p4;
+		x_position = x;
+		y_position = y;
+	}
 }
