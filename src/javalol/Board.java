c913238cf1d8434659e5412b1ff74a40a8556002
Daehyun Nam <namdhyun@gmail.com>
2014-05-26 23:27:43 +0900

    replaced long code with array to make code short

--- src/javalol/Board.java
+++ src/javalol/Board.java
@@ -38,105 +38,63 @@ public class Board extends JFrame implements ActionListener{
 	//이미지 배치할 좌표 지정해둔 정수형 배열.
 	int places[][] = {
 		{690, 670}, {610, 670}, {530, 670}, {450, 670}, {370, 670}, {290, 670}, {210, 670}, {130, 670}, {50, 670},//밑 가로줄 
-		{50, 590}, {50, 510}, {50, 430}, {50, 350}, {50, 270}, {50, 190}, {50, 110}, {50, 30}, //왼쪽 세로줄 끝
-		{130, 30}, {210, 30}, {290, 30}, {370, 30}, {450, 30}, {530, 30}, {610, 30}, {690, 30}, //위 가로줄 끝
-		{690, 110}, {690, 190}, {690, 270}, {690, 350}, {690, 430}, {690, 510}, {690, 590} //오른쪽 세로줄 끝
+		{50, 590}, {50, 510}, {50, 430}, {50, 350}, {50, 270}, {50, 190}, {50, 110}, {50, 30}, //왼쪽 세로줄
+		{130, 30}, {210, 30}, {290, 30}, {370, 30}, {450, 30}, {530, 30}, {610, 30}, {690, 30}, //위 가로줄
+		{690, 110}, {690, 190}, {690, 270}, {690, 350}, {690, 430}, {690, 510}, {690, 590} //오른쪽 세로줄
 	};
 	
 	//이미지 아이콘을 저장해둔 배열.
 	ImageIcon[] image = {
-		new ImageIcon("images/white.jpeg"),
+		//밑 가로줄
+		new ImageIcon("images/white.jpeg"), //Start Position
 		new ImageIcon("images/Alistar.png"),
 		new ImageIcon("images/Garen.png"),
 		new ImageIcon("images/Zac.png"),
-		new ImageIcon(""),
-		new ImageIcon(""),
-		new ImageIcon(""),
-		new ImageIcon(""),
-		new ImageIcon(""),
-		new ImageIcon(""),
-		new ImageIcon(""),
-		new ImageIcon(""),
-		new ImageIcon(""),
+		new ImageIcon("images/white.jpeg"), //Chance Card
+		new ImageIcon("images/Heimerdinger.png"),
+		new ImageIcon("images/Darius.png"),
+		new ImageIcon("images/Caitlyn.png"),
+		new ImageIcon("images/white.jpeg"), //Disconnect
+		//왼쪽 세로줄
+		new ImageIcon("images/Ryze.png"),
+		new ImageIcon("images/TwistedFate.png"),
+		new ImageIcon("images/white.jpeg"), //Chance Card
+		new ImageIcon("images/Vladimir.png"),
+		new ImageIcon("images/white.jpeg"), //Chance Card
+		new ImageIcon("images/Graves.png"),
+		new ImageIcon("images/Lucian.png"),
+		new ImageIcon("images/white.jpeg"), //Chance Card
+		//위 가로줄
+		new ImageIcon("images/Khazix.png"),
+		new ImageIcon("images/Renekton.png"),
+		new ImageIcon("images/white.jpeg"), //Chance Card
+		new ImageIcon("images/Lissandra.png"),
+		new ImageIcon("images/Nidalee.png"),
+		new ImageIcon("images/Velkoz.png"),
+		new ImageIcon("images/Ahri.png"),
+		new ImageIcon("images/white.jpeg"), //Chance Card
+		//오른쪽 세로줄
+		new ImageIcon("images/Brand.png"),
+		new ImageIcon("images/Annie.png"),
+		new ImageIcon("images/Syndra.png"),
+		new ImageIcon("images/white.jpeg"), //Chance Card
+		new ImageIcon("images/Katarina.png"),
+		new ImageIcon("images/DrMundo.png"),
+		new ImageIcon("images/Draven.png"),
 	};
 	
 	//이미지 아이콘을 감싸줄 레이블을 저장해둔 배열.
+	//iLabel[0] = new JLabel("images/white.jpeg");
 	JLabel[] imageLabel = { 
-		new JLabel(image[0]),
-		new JLabel(image[1]),
-		new JLabel(image[2])
+			new JLabel(image[0]), new JLabel(image[1]), new JLabel(image[2]), new JLabel(image[3]),
+			new JLabel(image[4]), new JLabel(image[5]), new JLabel(image[6]), new JLabel(image[7]),
+			new JLabel(image[8]), new JLabel(image[9]), new JLabel(image[10]), new JLabel(image[11]),
+			new JLabel(image[12]), new JLabel(image[13]), new JLabel(image[14]), new JLabel(image[15]),
+			new JLabel(image[16]), new JLabel(image[17]), new JLabel(image[18]), new JLabel(image[19]),
+			new JLabel(image[20]), new JLabel(image[21]), new JLabel(image[22]), new JLabel(image[23]),
+			new JLabel(image[24]), new JLabel(image[25]), new JLabel(image[26]), new JLabel(image[27]),
+			new JLabel(image[28]), new JLabel(image[29]), new JLabel(image[30]), new JLabel(image[31])
 	};
-	
-	ImageIcon image0 = new ImageIcon("images/white.jpeg"); //Start Position
-	JLabel imageLabel0 = new JLabel(image0);
-	ImageIcon image1 = new ImageIcon("images/Alistar.png");
-	JLabel imageLabel1 = new JLabel(image1);
-	ImageIcon image2 = new ImageIcon("images/Garen.png");
-	JLabel imageLabel2 = new JLabel(image2);
-	ImageIcon image3 = new ImageIcon("images/Zac.png");
-	JLabel imageLabel3 = new JLabel(image3);
-	ImageIcon image4 = new ImageIcon("images/white.jpeg"); //Chance Card
-	JLabel imageLabel4 = new JLabel(image4);
-	ImageIcon image5 = new ImageIcon("images/Heimerdinger.png");
-	JLabel imageLabel5 = new JLabel(image5);
-	ImageIcon image6 = new ImageIcon("images/Darius.png");
-	JLabel imageLabel6 = new JLabel(image6);
-	ImageIcon image7 = new ImageIcon("images/Caitlyn.png");
-	JLabel imageLabel7 = new JLabel(image7);
-	ImageIcon image8 = new ImageIcon("images/white.jpeg"); //Disconnect
-	JLabel imageLabel8 = new JLabel(image8);
-	//밑 가로줄 끝
-	
-	ImageIcon image10 = new ImageIcon("images/Ryze.png");
-	JLabel imageLabel10 = new JLabel(image10);
-	ImageIcon image11 = new ImageIcon("images/TwistedFate.png");
-	JLabel imageLabel11 = new JLabel(image11);
-	ImageIcon image12 = new ImageIcon("images/white.jpeg"); //Chance Card
-	JLabel imageLabel12 = new JLabel(image12);
-	ImageIcon image13 = new ImageIcon("images/Vladimir.png");
-	JLabel imageLabel13 = new JLabel(image13);
-	ImageIcon image14 = new ImageIcon("images/white.jpeg"); //Chance Card
-	JLabel imageLabel14 = new JLabel(image14);
-	ImageIcon image15 = new ImageIcon("images/Graves.png"); 
-	JLabel imageLabel15 = new JLabel(image15);
-	ImageIcon image16 = new ImageIcon("images/Lucian.png");
-	JLabel imageLabel16 = new JLabel(image16);
-	ImageIcon image17 = new ImageIcon("images/white.jpeg"); //Chance Card
-	//왼쪽 새로줄 끝
-	
-	JLabel imageLabel17 = new JLabel(image17);
-	ImageIcon image18 = new ImageIcon("images/Khazix.png");
-	JLabel imageLabel18 = new JLabel(image18);
-	ImageIcon image19 = new ImageIcon("images/Renekton.png");
-	JLabel imageLabel19 = new JLabel(image19);
-	ImageIcon image20 = new ImageIcon("images/white.jpeg"); //Chance Card
-	JLabel imageLabel20 = new JLabel(image20);
-	ImageIcon image21 = new ImageIcon("images/Lissandra.png");
-	JLabel imageLabel21 = new JLabel(image21);
-	ImageIcon image22 = new ImageIcon("images/Nidalee.png");
-	JLabel imageLabel22 = new JLabel(image22);
-	ImageIcon image23 = new ImageIcon("images/Velkoz.png");
-	JLabel imageLabel23 = new JLabel(image23);
-	ImageIcon image24 = new ImageIcon("images/Ahri.png");
-	JLabel imageLabel24 = new JLabel(image24);
-	ImageIcon image25 = new ImageIcon("images/white.jpeg"); //Chance Card
-	//위 가로줄 끝
-	
-	JLabel imageLabel25 = new JLabel(image25);
-	ImageIcon image26 = new ImageIcon("images/Brand.png");
-	JLabel imageLabel26 = new JLabel(image26);
-	ImageIcon image27 = new ImageIcon("images/Annie.png");
-	JLabel imageLabel27 = new JLabel(image27);
-	ImageIcon image28 = new ImageIcon("images/Syndra.png");
-	JLabel imageLabel28 = new JLabel(image28);
-	ImageIcon image29 = new ImageIcon("images/white.jpg"); //Chance Card
-	JLabel imageLabel29 = new JLabel(image29);
-	ImageIcon image30 = new ImageIcon("images/Katarina.png");
-	JLabel imageLabel30 = new JLabel(image30);
-	ImageIcon image31 = new ImageIcon("images/DrMundo.png");
-	JLabel imageLabel31 = new JLabel(image31);
-	ImageIcon image32 = new ImageIcon("images/Draven.png");
-	JLabel imageLabel32 = new JLabel(image32);
 	
 	ImageIcon image33 = new ImageIcon("images/test.png");
 	JLabel imageLabel33 = new JLabel(image33);
@@ -155,11 +113,11 @@ public class Board extends JFrame implements ActionListener{
 		
 		
 		//전반적인 레이아웃 짜는 부분
-		for(int i = 0; i < 33; i++){
-			for(int j = 0; j < 2; j++){
-			}
+		for(int i = 0; i < 32; i++){
+			createImage(imageLabel[i], places[i][0], places[i][1]);
 		}
 		
+		/*
 		createImage(imageLabel1, 690, 670);
 		createImage(imageLabel2, 610, 670);
 		createImage(imageLabel3, 530, 670);
@@ -192,6 +150,7 @@ public class Board extends JFrame implements ActionListener{
 		createImage(imageLabel30, 690, 430);
 		createImage(imageLabel31, 690, 510);
 		createImage(imageLabel32, 690, 590);
+		*/
 		
 		imageLabel33.setLocation(400, 400);
 		imageLabel33.setSize(20, 20);
