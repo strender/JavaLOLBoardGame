ba30c3350f06ed6eac23bf0e9cd53ce2c287014c
Daehyun Nam <namdhyun@gmail.com>
2014-06-07 15:59:50 +0900

    made some basic damage and price array

--- src/javalol/Block.java
+++ src/javalol/Block.java
@@ -4,21 +4,21 @@ public class Block{
 	String location_name;
 	boolean is_special;
 	int[] price = new int[5];
+	int[] damage = new int[5];
 	int x_position, y_position;
+	int owned = 0;
+	//0 == no one, 1 == player1, 2 == player2
 	
-	public Block(){
-		
+	public Block(String s, boolean b){
+		location_name = s;
+		is_special = b;
 	}
 	
-	public Block(String s, boolean special, int p0, int p1, int p2, int p3, int p4, int x, int y){
+	
+	public Block(String s, boolean b, int p1, int p2, int p3, int p4, int p5, int d1, int d2, int d3, int d4, int d5){
 		location_name = s;
-		is_special = special;
-		price[0] = p0;
-		price[1] = p1;
-		price[2] = p2;
-		price[3] = p3;
-		price[4] = p4;
-		x_position = x;
-		y_position = y;
+		is_special = b;
+		price[0] = p1; price[1] = p2; price[2] = p3; price[3] = p4; price[4] = p5;
+		damage[0] = d1; damage[1] = d2; damage[2] = d3; damage[3] = d4; damage[4] = d5;
 	}
 }
--- src/javalol/PHManager.java
+++ src/javalol/PHManager.java
@@ -1,5 +1,39 @@
 package javalol;
 
 public class PHManager {
-
+	
+	Block[] block_name = {
+		new Block("시작점", true),
+		new Block("알리스타", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("가렌", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("자크", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("찬스카드", true),
+		new Block("하이머딩거", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("다리우스", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("케이틀린", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("찬스카드", true),
+		new Block("라이즈", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("트페", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("찬스카드", true),
+		new Block("블라디미르", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("찬스카드", true),
+		new Block("그레이브즈", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("루시안", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("찬스카드", true),
+		new Block("카직스", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("레넥톤", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("찬스카드", true),
+		new Block("리산드라", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("니달리", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("벨코즈", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("아리", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("찬스카드", true),
+		new Block("브랜드", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("애니", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("신드라", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("찬스카드", true),
+		new Block("카타리나", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("문도박사", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+		new Block("드레이븐", false, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+	};
 }
